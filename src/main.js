ea22adb43b7bc36f455d6b8630d52df2ca107ff1
Ghosh <aghosh1@cmh.edu>
2018-08-26 13:05:11 -0500

    Initial test setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 require("babel-runtime/regenerator");
+require("babel-register");
 require("webpack-hot-middleware/client?reload=true");
 require("./main.css");
 require("./index.html");
-const a = async args => {
-  const { a, b } = args;
-  await console.log("hi");
-  console.log("done");
-};
-a({ a: 1, b: 2 });
+require("./index");
