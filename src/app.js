e340f056ad23775c893fe4c9f0786d0ca9375ef6
CMH\aghosh1 <aghosh1@cmh.edu>
2018-09-14 12:11:22 -0500

    react boiler plate

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import "./app.css";
+import Counter from "./counter";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <h1>Hello Pathyee</h1>
+        <Counter />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
-ReactDOM.render(<App />, document.getElementById("react-root"));
+// import App from "./app";
+import Counter from "./counter";
+import { AppContainer } from "react-hot-loader";
+function render(Component) {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById("react-root")
+  );
+}
+render(Counter);
+// ReactDOM.render(<Counter />, document.getElementById("react-root"));
+if (module.hot) {
+  // Code forr enabling stateful hot reloading in react
+  module.hot.accept("./counter.js", () => {
+    const newCounter = require("./counter.js").default;
+    render(newCounter);
+  });
+}
