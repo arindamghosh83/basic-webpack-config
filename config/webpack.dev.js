4125fc541c8b6ec42d9a506c000b182f3d3e5cc4
CMH\aghosh1 <aghosh1@cmh.edu>
2018-09-14 14:16:34 -0500

    hooking up css preprocessorrs like sass

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -42,6 +42,20 @@ module.exports = {
         ]
       },
       {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
         test: /\.html$/,
         use: [
           // {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ require("babel-runtime/regenerator");
 require("react-hot-loader/patch");
 require("babel-register");
 require("webpack-hot-middleware/client?reload=true");
-require("./main.css");
+require("./main.sass");
 require("./index.html");
 require("./index");
